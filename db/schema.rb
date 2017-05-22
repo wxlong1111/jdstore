89495be797b240f3644165acaa8326173e48eae7
wxlong <wxlong1111@163.com>
2017-05-23 05:50:37 +0800

    rails g migration add_image_to_product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521065927) do
+ActiveRecord::Schema.define(version: 20170522214859) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170521065927) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
